36ec2c5a433c1aaaed093916a0b843ed731e1321
Martin Finkel <finkel2804@gmail.com>
2020-08-29 21:21:40 +0200

    add Microsoft.Build ref and new benchmark file

---
+++ src/MSBuild.Benchmarks/BenchmarkTest.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Build.Tasks;
+
+namespace MSBuild.Benchmarks
+{
+    public class BenchmarkTest
+    {
+        [Benchmark]
+        public void Test()
+        {
+            ResolveAssemblyReference t = new ResolveAssemblyReference();
+
+            //t.Execute();
+        }
+    }
+}
