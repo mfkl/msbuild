d41c5711801a30fdb9a150243bbfdcc0ac486552
Martin Finkel <finkel2804@gmail.com>
2020-09-05 19:07:34 +0200

    add preprocessor benchmarks

--- src/MSBuild.Benchmarks/BenchmarkTest.cs
+++ src/MSBuild.Benchmarks/BenchmarkTest.cs
@@ -1,16 +1,51 @@
 ﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
 using Microsoft.Build.Tasks;
+using System.IO;
 
 namespace MSBuild.Benchmarks
 {
     public class BenchmarkTest
     {
-        [Benchmark]
-        public void Test()
-        {
-            ResolveAssemblyReference t = new ResolveAssemblyReference();
+        Project project;
 
-            //t.Execute();
+        //[Benchmark]
+        //public void Test()
+        //{
+        //    ResolveAssemblyReference t = new ResolveAssemblyReference();
+        //    t.Execute();
+        //}
+
+        [GlobalSetup(Target = nameof(PreprocessorBenchmarkSingle))]
+        public void PreprocessorBenchmarkSingleSetup()
+        {
+            project = new Project();
+            project.SetProperty("p", "v1");
+        }
+
+        [GlobalSetup(Target = nameof(PreprocessorBenchmarkInitialTargetsOuterAndInner))]
+        public void PreprocessorBenchmarkInitialTargetsOuterAndInnerSetup()
+        {
+            ProjectRootElement xml1 = ProjectRootElement.Create("p1");
+            xml1.InitialTargets = "i1";
+            xml1.AddImport("p2");
+            ProjectRootElement xml2 = ProjectRootElement.Create("p2");
+            xml2.InitialTargets = "i2";
+
+            project = new Project(xml1);
+        }
+
+        [Benchmark]
+        public void PreprocessorBenchmarkSingle()
+        {
+            project.SaveLogicalProject(new StringWriter());
+        }
+
+        [Benchmark]
+        public void PreprocessorBenchmarkInitialTargetsOuterAndInner()
+        {
+            project.SaveLogicalProject(new StringWriter());
         }
     }
 }
